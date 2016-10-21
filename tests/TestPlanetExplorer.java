2ff9a567455c4e52c1b2fa6bd25cce682a5af98d
somename <someemail>
2016-10-21 16:09:53 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -4,11 +4,6 @@ import org.junit.Test;
 
 public class TestPlanetExplorer {
 	char direction;
-
-	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
-	}
 	@Test
 	public void test_Facing_North_on_start() {
 		PlanetExplorer rover = new PlanetExplorer(500,500, "idk");
