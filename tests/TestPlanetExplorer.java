2a9efe4c05b8b4cb851e3de10c125d46b192798c
somename <someemail>
2016-10-21 16:04:31 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -13,7 +13,7 @@ public class TestPlanetExplorer {
 	public void test_Facing_North_on_start() {
 		PlanetExplorer rover = new PlanetExplorer(500,500, "idk");
 		direction = rover.getDirection();
-		assertEquals("Should be facing North", 'W', direction);
+		assertEquals("Should be facing North", 'N', direction);
 	}
 	@Test
 	public void test_turnPlanetExplorerLeft(){
